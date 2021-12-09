606c7ebf2e3ee36d77a4c4833c392f9f15eccb71
wat <wat@DESKTOP-5DRGLU6>
2021-12-09 13:49:56 +0900

    초기 커밋.

---
+++ WebApplication/Controllers/HomeController.cs
@@ -0,0 +1,79 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+using WebApplication.Models.Common;
+
+namespace WebApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            string result = GetResults(1, 315);
+            var r = JObject.Parse(result);
+            var list = r["data"];
+            JToken shuffle = list.OrderBy(g => Guid.NewGuid()).FirstOrDefault();
+            ViewBag.shuffle = shuffle;
+
+            return View();
+        }
+
+        public ActionResult About(int? page)
+        {
+            if (page == null)
+            {
+                page = 1;
+            }
+            string result = GetResults((int)page, 10);
+            var r = JObject.Parse(result);
+            var list = r["data"];
+            int totalCount = Convert.ToInt32(r["totalCount"]);
+            int perPage = Convert.ToInt32(r["perPage"]);
+            int totalPage = totalCount % perPage == 0 ? totalCount / perPage : totalCount / perPage + 1;
+            if (page > totalPage || page <= 0)
+            {
+                Response.Write("<script>alert('잘못된 페이지입니다.')</script>");
+                page = 1;
+            }
+            result = GetResults((int)page, 10);
+            r = JObject.Parse(result);
+            list = r["data"];
+
+            ViewBag.totalPage = totalPage;
+            ViewBag.list = list;
+            ViewBag.page = page;
+
+            return View();
+        }
+
+        private static string GetResults(int page, int perPage)
+        {
+            string result = string.Empty;
+            try
+            {
+                WebClient client = new WebClient();
+                string url = string.Format(@"{0}?serviceKey={1}&page={2}&perPage={3}", Consts.targetURL, Consts.serviceKey, page.ToString(), perPage.ToString());
+                using (Stream data = client.OpenRead(url))
+                {
+                    using (StreamReader reader = new StreamReader(data))
+                    {
+                        string s = reader.ReadToEnd();
+                        result = s;
+
+                        reader.Close();
+                        data.Close();
+                    }
+                }
+            }
+            catch (Exception)
+            {
+
+            }
+
+            return result;
+        }
+    }
+}
---
+++ WebApplication/Models/Common/Consts.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApplication.Models.Common
+{
+    public class Consts
+    {
+        public const string targetURL = "https://api.odcloud.kr/api/3053840/v1/uddi:9cb85e53-3592-4a8d-9b07-eec77edbc8c2";
+        public const string serviceKey = "mwi9j7b19r%2BD93GDSpt8iA6yVpXrD0G1d2qLBtUwhMdwz1JOs14HULqe7tn39PHELQ9sCCAoIYJUorpgnW0wsw%3D%3D";
+    }
+}
